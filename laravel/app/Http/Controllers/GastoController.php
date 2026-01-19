b2e6e121fbf05de2e0d701505e406242b0d3c4e9
ikerea <ikereguizabal@gmail.com>
2026-01-19 09:08:12 +0100

    Crud de gastos hecho

--- laravel/app/Http/Controllers/GastoController.php
+++ laravel/app/Http/Controllers/GastoController.php
@@ -16,4 +16,42 @@ class GastoController extends Controller {
         
         return Inertia::render('gastos', compact("gastos"));
     }
+
+    public function addGasto(Request $request){
+
+        $datosProcesados = $request->validate([
+        'nombre' => 'required|string|max:255',    
+        'dinero' => 'required|numeric|min:0',  
+        'fecha'  => 'required|date',            
+        'pagado' => 'boolean',                 
+    ]);
+
+    Gasto::create($datosProcesados);
+
+    return redirect()->back();
+    }
+
+    public function editGasto(Request $request, Gasto $gasto) {
+
+        $datosProcesados = $request->validate([
+            'nombre' => 'required|string|max:255',    
+            'dinero' => 'required|numeric|min:0',  
+            'fecha'  => 'required|date',            
+            'pagado' => 'boolean',                 
+        ]);
+        
+        $gasto->update($datosProcesados);
+
+        return redirect()->route('gastos.index')
+        ->with('message', 'Gasto actualizado correctamente');
+    }
+
+    public function eliminarGasto(Gasto $gasto) {
+
+        $gasto->delete();
+
+        return redirect()->route('gastos.index');
+    }
+
+
 }
