4a214bdc5a1291f6a872e93c890c3f8d305c8c6f
ikerea <ikereguizabal@gmail.com>
2026-02-04 12:27:17 +0100

    Primera version del despliegue bien

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!--@routes-->
+        @vite(['resources/js/app.tsx'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
@@ -12,23 +18,36 @@
 
         <script>
             window.interdeal = {
-                "sitekey": "38c73bbde50343409cd42939d229369a", // Tu clave real
+                get sitekey (){ return "7e5aa89f18a26474bb2786c02e3815d0"} ,
+                get domains(){
+                    return {
+                        "js": "https://cdn.equalweb.com/",
+                        "acc": "https://access.equalweb.com/"
+                    }
+                },
                 "Position": "left",
                 "Menulang": "ES",
-                "domains": {
-                    "js": "https://cdn.equalweb.com/",
-                    "acc": "https://access.equalweb.com/"
-                },
+                "draggable": true,
                 "btnStyle": {
-                    "vPosition": ["80%", "80%"],
-                    "scale": ["0.5", "0.5"],
-                    "color": { "main": "#1c4bb6", "second": "#ffffff" },
-                    "icon": { "type": 1, "shape": "circle" }
-                }
+                    "vPosition": [ "80%", "80%" ],
+                    "margin": [ "0", "0" ],
+                    "scale": [ "0.5", "0.5" ],
+                    "color": {
+                        "main": "#1c4bb6",
+                        "second": "#ffffff"
+                    },
+                    "icon": {
+                        "outline": false,
+                        "outlineColor": "#ffffff",
+                        "type": 1 ,
+                        "shape": "circle"
+                    }
+                },
             };
+
             (function(doc, head, body){
                 var coreCall = doc.createElement('script');
-                coreCall.src = 'https://cdn.equalweb.com/core/5.2.5/accessibility.js';
+                coreCall.src = interdeal.domains.js + 'core/5.2.5/accessibility.js';
                 coreCall.defer = true;
                 coreCall.integrity = 'sha512-Zamp30ps601kXvZTcIYv1sytUc090mrEJD9rLuoWzEGqmB6t0XdLRgC/g5TznUleEBIMm6T3c6Baf/ExIYh/Hw==';
                 coreCall.crossOrigin = 'anonymous';
@@ -36,5 +55,5 @@
                 body? body.appendChild(coreCall) : head.appendChild(coreCall);
             })(document, document.head, document.body);
         </script>
-        </body>
+    </body>
 </html>
